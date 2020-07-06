b709ce0bc5e7cffbf5e73996b16b1abf34d10471
Omar Guajardo <omarguajardo@fb.com>
2020-07-06 10:08:02 -0700

    able to retrieve info from Parse server

--- app/src/main/java/com/example/instaparse/MainActivity.java
+++ app/src/main/java/com/example/instaparse/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.instaparse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.instaparse.databinding.ActivityMainBinding;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String TAG = "MainActivity";
+
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        
+        queryPosts();
+
+    }
+
+    private void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "error with querying posts", e );
+                    return;
+                }
+                for(Post post: posts){
+                    Log.d(TAG, "this is the post desc " + post.getDescription() + " username " +post.getUser().getUsername());
+                }
+            }
+        });
     }
 }
